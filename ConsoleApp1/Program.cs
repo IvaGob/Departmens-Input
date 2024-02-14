0adc224fa8e32b90c00b7e6c760856ad8e88a6e6
Ivan <138679745+IvaGob@users.noreply.github.com>
2024-02-14 23:29:15 +0200

    Update Program.cs
    Changed program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 
 namespace ConsoleApp1
 {
@@ -7,9 +7,26 @@ namespace ConsoleApp1
 
     public class Department
     {
-        public string name;
-        public int plannedTime;
-        public int factTime;
+        private string name { get; set; }
+
+        private int _plannedTime = 0;
+        int plannedTime
+        {
+            get => _plannedTime;
+            set
+            {
+                if (value >= 0) _plannedTime = value;
+            }
+        }
+        private int _factTime = 0;
+        int factTime
+        {
+            get => _factTime;
+            set
+            {
+                if (value >= 0) _factTime = value;
+            }
+        }
         public int deviationInHours()
         {
             return plannedTime - factTime;
@@ -18,21 +35,93 @@ namespace ConsoleApp1
         {
             return (deviationInHours() * 100) / plannedTime;
         }
-
+        public int GetPlannedTime()
+        {
+            return plannedTime;
+        }
+        public int GetFactTime()
+        {
+            return factTime;
+        }
         public Department(string name, int plannedTime, int factTime)
         {
             this.name = name;
             this.plannedTime = plannedTime;
             this.factTime = factTime;
         }
+        public virtual void Display()
+        {
+            Console.WriteLine("Кафедра:" + this.name +
+                               "\n\tВикористання машинного часу" +
+                               "\n\t\tЗа планом: " + this.plannedTime +
+                               "\n\t\tФактично: " + this.factTime +
+                               "\n\tВідхилення від плану" +
+                               "\n\t\tУ годинах: " + this.deviationInHours() +
+                               "\n\t\t У %: " + this.deviationInPercent());
+        }
     }
+    class AllDepartments : Department
+    {
+        private int _plannedTime = 0;
+        int plannedTime { get => _plannedTime;
+            set 
+            {
+                if(value>=0)_plannedTime = value;
+            } 
+        }
+        private int _factTime= 0;
+        int factTime { get => _factTime;
+            set
+            {
+                if (value >= 0) _factTime = value;
+            }
+        }
+        public AllDepartments(int plannedTime, int factTime) : base("разом", plannedTime, factTime)
+        {
+            this.plannedTime= plannedTime;
+            this.factTime= factTime;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Разом:"+
+                              "\n\tВикористання машинного часу" +
+                              "\n\t\tЗа планом: " + this.plannedTime +
+                              "\n\t\tФактично: " + this.factTime +
+                              "\n\tВідхилення від плану" +
+                              "\n\t\tУ годинах: " + this.deviationInHours() +
+                              "\n\t\t У %: " + this.deviationInPercent());
+        }
+    }
+
     internal class Program
     {
        
         static void Main(string[] args)
         {
+            Console.InputEncoding = System.Text.Encoding.Unicode;
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             List<Department> arr = new List<Department>();
+            Input(arr);
+
+            int plannedTimeSum =0;
+            int factTimeSum = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                plannedTimeSum += arr[i].GetPlannedTime();
+                factTimeSum += arr[i].GetFactTime();
+                
+                Console.WriteLine("("+Convert.ToString(i+1)+")");
+                arr[i].Display();
+            }
+            AllDepartments all = new AllDepartments(plannedTimeSum,factTimeSum);
+            all.Display();
+            
+        }
+        //Ввід данних
+        static void Input(List<Department> arr)
+        {
             //Цикл вводу данних кафедр
             do
             {
@@ -42,36 +131,14 @@ namespace ConsoleApp1
                 string InputName = Console.ReadLine();
                 //Ввід запланованих годин
                 Console.WriteLine("Введіть заплановані години:");
-                string InputPlannedTime = Console.ReadLine();
+                int InputPlannedTime = Convert.ToInt32(Console.ReadLine());
                 //Ввід фактичних годин
                 Console.WriteLine("Введіть фактичні новини години:");
-                string InputFactTime = Console.ReadLine();
+                int InputFactTime = Convert.ToInt32(Console.ReadLine());
                 //Створення нового екземпляра класу Department і його додавання в масив arr
-                arr.Add(new Department(InputName, Convert.ToInt32(InputPlannedTime), Convert.ToInt32(InputFactTime)));
+                arr.Add(new Department(InputName, InputPlannedTime, InputFactTime));
                 Console.WriteLine("Продовжити ввід кафедр? (y/n) ");
             } while (Console.ReadLine() == "y");
-            int plannedTimeSum =0;
-            int factTimeSum = 0;
-            for (int i = 0; i < arr.Count; i++)
-            {
-                plannedTimeSum+=arr[i].plannedTime;
-                factTimeSum += arr[i].factTime;
-                Console.WriteLine("("+Convert.ToString(i+1)+")");
-                Display(arr[i]);
-            }
-            Department sum = new Department("Разом",plannedTimeSum,factTimeSum);
-            Display(sum);
-            //Виведення данних кафедри
-            void Display(Department department)
-            {
-                Console.WriteLine("Кафедра:" + department.name +
-                                   "\n\tВикористання машинного часу" +
-                                   "\n\t\tЗа планом: " + department.plannedTime +
-                                   "\n\t\tФактично: " + department.factTime +
-                                   "\n\tВідхилення від плану" +
-                                   "\n\t\tУ годинах: " + department.deviationInHours() +
-                                   "\n\t\t У %: " + department.deviationInPercent());
-            }
         }
     }
 }
